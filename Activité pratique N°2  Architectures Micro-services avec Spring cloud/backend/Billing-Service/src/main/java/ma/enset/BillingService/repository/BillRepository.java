8b6d198cdf33fed2a626c899951022772c2b1d68
hassanouado <ouadochhassan2001@gmail.com>
2023-12-23 18:31:36 +0100

    change in the TP 2

--- Activité pratique N°2  Architectures Micro-services avec Spring cloud/backend/Billing-Service/src/main/java/ma/enset/BillingService/repository/BillRepository.java
+++
@@ -1,9 +0,0 @@
-package ma.enset.BillingService.repository;
-
-import ma.enset.BillingService.entities.Bill;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface BillRepository extends JpaRepository<Bill, Long> {
-}
